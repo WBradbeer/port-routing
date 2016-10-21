f34e51a8db4c5277bb31e1f558deb41fb14d8e80
Will Bradbeer <willbradbeer@gmail.com>
2016-10-20 21:08:57 -0400

    Added containers_sent to calc total impact of routing.

--- graph_helpers.py
+++ graph_helpers.py
@@ -1,23 +1,30 @@
 import networkx as nx
 
 
-def df_to_edges(cost_df):
+def df_to_edges(df):
     edges = []
-    for port in cost_df.index.values:
-        for dest in cost_df.index.values:
-            if cost_df[port][dest] > 0:
-                edges.append((port,dest,{'cost': cost_df[port][dest]}))
+    for source in df.index.values:
+        for dest in df.index.values:
+            if df[source][dest] > 0:
+                edges.append((source, dest, {'cost': df[source][dest]}))
     return edges
 
 
-def scanning_path(G,source, scanning_ports):
+def scanning_path(G, source, scanning_ports):
     min_path = None
     min_cost = None
     cost, path = nx.single_source_dijkstra(G, source, weight="cost",
                                            cutoff=min_cost)
-    for sp, sc in scanning_ports:
+    for sp, sc in scanning_ports.itertuples():
         cost[sp] += sc
         if not min_cost or min_cost > cost[sp]:
             min_cost = cost[sp]
             min_path = path[sp]
     return min_path, min_cost
+
+
+def total_container_cost(G, containers_sent, scanning_ports):
+    total_cost = 0
+    for port, num in containers_sent.itertuples():
+        total_cost += scanning_path(G, port, scanning_ports)[1] * num
+    return total_cost
--- port_ex.py
+++ port_ex.py
@@ -1,19 +1,25 @@
+import os
+
 import networkx as nx
 import pandas as pd
 
 import graph_helpers as gh
 
-G = nx.DiGraph()
 
-edge_costs = pd.read_csv("port_costs.csv", index_col="Ports")
+file_path = os.path.abspath(os.path.dirname(__file__))
+
+containers_sent = pd.read_csv(file_path + "/data/containers_sent.csv", index_col="Port")
+scanning_ports = pd.read_csv(file_path + "/data/scanning_ports.csv", index_col="Port")
+edge_costs = pd.read_csv(file_path + "/data/port_costs.csv", index_col="Port")
 ports = edge_costs.index.values
 edges = gh.df_to_edges(edge_costs)
 
+G = nx.DiGraph()
 G.add_nodes_from(ports)
 G.add_edges_from(edges)
-
-scanning_ports = [("Halifax", 5)]
 
 
 path = gh.scanning_path(G, ports[1], scanning_ports)
 print path
+
+print gh.total_container_cost(G, containers_sent, scanning_ports)
